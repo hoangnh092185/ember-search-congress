c0fc98a6074ab5ef476f96cc293ba916deddcf84
Nhat Hoang and Timothy Herold <nhat@>
2016-11-01 16:12:46 -0700

    add comming up bill display.

--- app/router.js
+++ app/router.js
@@ -9,11 +9,11 @@ Router.map(function() {
   this.route('results', {path: '/results/:zip'});
   this.route('committee', {path: '/committee/:page'});
   this.route('subcommittee', {path: '/subcommittee/:committee_id'});
-  // this.route('bills', {path: '/bills/:keywords'});
   this.route('bills');
 
   this.route('billsearch', {path: '/billsearch/:keywords/:page'});
   this.route('billdetail', {path: '/billdetail/:bill_id'});
+  this.route('upcomingbill');
 });
 
 export default Router;
