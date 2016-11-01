d3ecfd7af26be7b03a870809a5050cecc775d700
Nhat Hoang and Timothy Herold <nhat@>
2016-11-01 16:40:01 -0700

    add sort by date to bill display.

--- app/components/bill-search.js
+++ app/components/bill-search.js
@@ -1,6 +1,9 @@
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['last_action_at'],
+  sortedBills: Ember.computed.sort('model.results', 'sortBy'),
   actions: {
     searchBill(){
       var params = { keywords: this.get('keywords')
